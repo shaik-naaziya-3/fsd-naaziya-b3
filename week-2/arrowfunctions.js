c8f64981eb4423da8f71f335c1f961e2cf1eb4e7
naaziya-demo <naaziyashaik1736@gmail.com>
2026-07-18 10:40:47 +0530

    Updated Programs

--- week-2/arrowfunctions.js
+++ week-2/arrowfunctions.js
@@ -1,27 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // Traditional Named Function
-function findTotal(price: number, quantity: number): number {
+function findTotal(price, quantity) {
     return price * quantity;
 }
-
-
 // Arrow Function
-const addTax = (total: number, tax: number): number => {
+const addTax = (total, tax) => {
     return total + tax;
 };
-
-
 // Shorthand Arrow Function
-const getStatus = (amount: number): string =>
-    amount >= 100 ? "Premium Order" : "Regular Order";
-
-
+const getStatus = (amount) => amount >= 100 ? "Premium Order" : "Regular Order";
 // Function Calls
 let total = findTotal(25, 4);
 let finalAmount = addTax(total, 10);
 let typeoforder = getStatus(finalAmount);
-
-
 // Output
 console.log(`Total:${total}`);
 console.log(`FinalAmount:${finalAmount}`);
 console.log(`TypeofOrder:${typeoforder}`);
+//# sourceMappingURL=arrowfunctions.js.map
